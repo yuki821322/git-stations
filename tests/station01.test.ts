65c5fcf4a3851284d8d343d75b133670734fcf42
Àżu <yamane_kota@techbowl.co.jp>
2024-06-11 03:01:15 +0900

    feat: station01テストを追加

---
+++ tests/station01.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from 'vitest'
+
+describe('Station01', () => {
+  it('リポジトリをクローンできている', () => {
+    // NOTE: テストが実行できている時点でクローンできている
+    console.log('Pass station01.')
+  })
+})
