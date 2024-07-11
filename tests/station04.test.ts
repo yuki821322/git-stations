7685752f5db72ceed5f4c3f36168baf21abb8f43
Àżu <yamane_kota@techbowl.co.jp>
2024-07-11 21:19:17 +0900

    fix: station04不足テストを追加

--- tests/station04.test.ts
+++ tests/station04.test.ts
@@ -2,6 +2,15 @@ import { describe, expect, it } from 'vitest'
 import simpleGit from 'simple-git'
 
 describe('Station04', () => {
+  it('feature/add-footerブランチがプッシュされている', async () => {
+    const expectedBranch = 'origin/feature/add-footer'
+    const git = simpleGit()
+    const branches = (await git.branch(['-r'])).all
+    const result = branches.some((branch) => branch === expectedBranch)
+
+    expect(result).toBeTruthy()
+  })
+
   it('feature/accessブランチが存在する', async () => {
     const expectedBranch = 'feature/access'
     const git = simpleGit()
