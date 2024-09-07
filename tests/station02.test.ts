20fff77e95ffbb3169ff97327ddb504c7aede761
Àżu <yamane_kota@techbowl.co.jp>
2024-09-07 14:24:09 +0900

    fix: station02テスト判定

--- tests/station02.test.ts
+++ tests/station02.test.ts
@@ -2,8 +2,8 @@ import { describe, it, expect } from 'vitest'
 import simpleGit from 'simple-git'
 
 describe('Station02', () => {
-  it('コミットメッセージ「fix: 営業日時の修正」のコミットが存在する', async () => {
-    const expectedMessage = 'fix: 営業日時の修正'
+  it('コミットメッセージ「fix: 営業日時」のコミットが存在する', async () => {
+    const expectedMessage = 'fix: 営業日時'
     const git = simpleGit()
     const logs = await git.log()
     const result = logs.all.some((log) => log.message === expectedMessage)
